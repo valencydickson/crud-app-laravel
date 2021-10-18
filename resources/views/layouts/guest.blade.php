0087763e6c836b3b7635e5e7619f0fee24a4e17b
valencydickson <dickson.valency@gmail.com>
2021-10-19 02:03:57 +0300

    add some changes

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+
+<body>
+
+    <header class="container mx-auto flex justify-between items-center p-5 bg-gray-100">
+        <h1 class="text-2xl font-semibold"><a href="{{url('/')}}">Products</a></h1>
+        <nav class="flex justify-end">
+            @if(Route::has("login"))
+
+            @auth
+            <a href="{{url('/dashboard')}}" class="pl-4">My Account</a>
+            <a href="{{url('/logout')}}" class="pl-4">Logout</a>
+
+            @else
+            <a href="{{route('login')}}" class="pl-4">Login</a>
+
+            @if(Route::has("register"))
+            <a href="{{route('register')}}" class="pl-4">Register</a>
+            @endif
+
+            @endauth
+            @endif
+        </nav>
+    </header>
+
+    <div>
+        {{ $slot }}
+    </div>
+</body>
+
+</html>
