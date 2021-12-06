21c8fd1db2520bb4cc4b077842f594cc7264c264
valencydickson <dickson.valency@gmail.com>
2021-12-06 08:42:26 +0300

    remove avatar

--- resources/views/components/user.blade.php
+++ resources/views/components/user.blade.php
@@ -1,24 +1,5 @@
 <div>
-    <div class="flex items-center">
-        @if (Auth::user()->avatar == NULL )
-        <img src="https://www.kindpng.com/picc/m/78-785827_user-profile-avatar-login-account-male-user-icon.png"
-            alt="Avatar" class="mr-6 avatar">
-        @else
-        <img src="{{asset('/storage/images/' . Auth::user()->avatar)}}" alt="Avatar" class="mr-6 avatar">
-        @endif
-        <div>
-            <form action="/upload" method="post" enctype="multipart/form-data">
-                @csrf
-                <input type="file" name="image"><br>
-                <span>Only up to 2 MB</span><br>
-                <input type="submit" value="Upload" class="mt-3 py-2 px-6 bg-blue-700 text-white">
-            </form>
-            @if(session()->has('uploadMessage'))
-            <p class="text-green-600">{{session("uploadMessage")}}</p>
-            @endif
-        </div>
-    </div>
-    <div class="mt-4">
+    <div class="mt-2">
         <div class="flex justify-between items-center mb-2">
             <div class="flex items-center">
                 <p class="mr-3 font-bold">Name: </p>
